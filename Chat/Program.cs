df72b38cac680d07923a7ac6f4e8c2f8dffadac4
Julien.Cottier <jujucottier@gmail.com>
2020-11-23 11:38:05 +0100

    Ex4 Facile ! Merci le prof !

--- Chat/Program.cs
+++ Chat/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 
 namespace Chat
 {
@@ -6,7 +9,33 @@ namespace Chat
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Serveur
+            try
+            {
+                Console.WriteLine("Serveur Chat");
+                TcpListener server = new TcpListener(
+                    IPAddress.Parse("0.0.0.0"), 9876
+                );
+                server.Start();
+
+                while (true)
+                {
+                    // Hang
+                    using (TcpClient client = server.AcceptTcpClient())
+                    {
+                        using (StreamReader reader = new StreamReader(client.GetStream()))
+                        {
+                            string text = reader.ReadLine();
+                            Console.WriteLine(text);
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
---
+++ Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static bool reconnexion = true;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ton nom !?");
+            string name = Console.ReadLine();
+            while (true)
+            {
+                TcpClient client = new TcpClient();
+                Console.WriteLine("Ecrie ton texte !");
+                try
+                {
+                    client.Connect("127.0.0.1", 9876);
+
+                    if (client.Connected)
+                    {
+                        NetworkStream stream = client.GetStream();
+                        StreamWriter writer = new StreamWriter(stream);
+                        string saisie = Console.ReadLine();
+                        writer.WriteLine(name + ": " + saisie);
+                        writer.Flush();
+                        writer.Dispose();
+
+                        //Console.ReadKey();
+                        client.Close();
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Erreur de connexion au serveur.");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    if (!reconnexion) break;
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
